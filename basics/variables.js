38aea75b2ae1b93256ec3b3eef02696150c5e965
Rick1196 <ricardompp11@gmail.com>
2022-09-09 22:02:56 -0500

    basics vairables v2

--- basics/variables.js
+++ basics/variables.js
@@ -32,7 +32,7 @@ variableValue = []; // replacing value
 const newObject = { ...object, ...{ a: 1 } };
 newObject[randomKey] = 1;
 //
-const newArray = [...array, ...[1,2]]; //[integer, string, object, 1, 2]
+const newArray = [...array, ...[1, 2]]; //[integer, string, object, 1, 2]
 
 // declaring an arrow function
 const getObjectKeysValues = (param) => {
@@ -51,6 +51,42 @@ const getObjectKeysValues = (param) => {
 // getObjectKeysValues(object);
 
 // export
+
+// undefined null 0 false
+const arrayWithItems = [];
+if (arrayWithItems !== undefined || arrayWithItems !== null) {
+  //...
+} else if (arrayWithItems.length === 0) {
+  arrayWithItems.push({});
+}
+
+// try catch
+const divide = (a, b) => {
+  try {
+    // main code
+    const result = a / 0;
+    return result;
+  } catch (error) {
+    console.log(error);
+    return "You cannot divide between 0";
+  }
+};
+
+// typeof
+let user = '';
+console.log(typeof user); // string
+user = {
+  email:'',
+  username: ''
+}
+console.log(typeof user); // object
+user = [];
+console.log(typeof user); // array
+user = () =>{}
+console.log(typeof user); // function
+
+
+
 exports.array = array;
 exports.object = object;
 exports.getObjectKeysValues = getObjectKeysValues;
