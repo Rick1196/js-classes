a220317ac4eaca3e6ceba2dcce59ba7b9e23f324
Rick1196 <ricardompp11@gmail.com>
2022-10-06 21:34:35 -0500

    tic tac toe

--- challenges/tic-tac-toe.mjs
+++ challenges/tic-tac-toe.mjs
@@ -1,11 +1,6 @@
 import readline from "readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const matrix = [
+let matrix = [
   [0, 0, 0],
   [0, 0, 0],
   [0, 0, 0],
@@ -20,15 +15,25 @@ const displayMatrix = (mat) => {
   }
 };
 
-const askInput = (prompt) => {
-  let input = "";
-  rl.question(prompt, function (answer) {
-    console.log("Thank you for your valuable feedback:", answer);
-    input = answer;
-    rl.close();
+const askInput = async (prompt) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
-  const coordinates = input.split(",");
-  return coordinates;
+  let response = "";
+  rl.setPrompt(prompt);
+  rl.prompt();
+
+  return new Promise((resolve, reject) => {
+    rl.on("line", (userInput) => {
+      response = userInput;
+      rl.close();
+    });
+
+    rl.on("close", () => {
+      resolve(response.split(","));
+    });
+  });
 };
 
 const validateCoordinates = (coordinates) => {
@@ -42,7 +47,7 @@ const validateCoordinates = (coordinates) => {
       errors.push(`${coord} is not a integer number`);
     } else {
       if (!(parseInt(coord, 10) < 3 && parseInt(coord, 10) >= 0)) {
-        error.push(`${coord} is not a valid coordinate`);
+        errors.push(`${coord} is not a valid coordinate`);
       }
     }
   }
@@ -64,14 +69,14 @@ const updateCoordenate = (coordinates, currentplayer, mat) => {
 };
 
 const transponseMatrix = (mat) => {
-  return mat[0].map((_, colIndex) => array.map((row) => row[colIndex]));
+  return mat[0].map((_, colIndex) => mat.map((row) => row[colIndex]));
 };
 
-const checkRows = (currentPlayer, mat) =>{
+const checkRows = (currentPlayer, mat) => {
   let isAWinner = false;
   for (const row of mat) {
     // [0,0,0]
-// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+    // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/some
     const resul = row.some((element) => element !== currentPlayer);
     if (!resul) {
       isAWinner = true;
@@ -79,35 +84,39 @@ const checkRows = (currentPlayer, mat) =>{
     }
   }
   return isAWinner;
-}
+};
 
 const checkDiagonal = (currentPlayer, mat) => {
   let change = true;
-  for(let x=0; x<mat.length; x++){
-    if(currentPlayer !== mat[x][x]){
+  for (let x = 0; x < mat.length; x++) {
+    if (currentPlayer !== mat[x][x]) {
       change = false;
       break;
     }
   }
   //cambiar a descendente
-  for(let x=0; x<mat.length; x++){
-    if(currentPlayer !== mat[x][x]){
-      change = false;
-      break;
+  if (change) {
+    for (let x = 2; x >= 0; x--) {
+      if (currentPlayer !== mat[x][x]) {
+        change = false;
+        break;
+      }
     }
   }
+
   return change;
-}
+};
 const isThereAWinner = (currentPlayer, mat) => {
   let isAWinner = false;
-  isAWinner = checkRows(mat);
-  if(!isAWinner){
+  isAWinner = checkRows(currentPlayer, mat);
+  if (!isAWinner) {
     const transponseMat = transponseMatrix([...mat]);
-    isAWinner = checkRows(transponseMat);
+    isAWinner = checkRows(currentPlayer, transponseMat);
   }
-  if(!isAWinner){
-    
+  if (!isAWinner) {
+    isAWinner = checkDiagonal(currentPlayer, mat);
   }
+  return isAWinner;
 
   // check row 0
   // check row 1
@@ -118,9 +127,10 @@ const isThereAWinner = (currentPlayer, mat) => {
   // check diagonal
 };
 
-const main = () => {
+const main = async () => {
   while (!winner) {
-    const coordinates = askInput(`Player ${player} Coordinates(x,y): `);
+    const coordinates = await askInput(`Player ${player} Coordinates(x,y): `);
+    console.log(coordinates);
     const errors = validateCoordinates(coordinates);
     if (errors.length === 0) {
       // function to replace the 0 to player return true or false
@@ -133,16 +143,31 @@ const main = () => {
         player,
         matrix
       );
+      console.log(wasUpdated);
       if (!wasUpdated) {
         console.log(`Coordinate occupied ${coordinates}`);
       } else {
         matrix = updatedMatrix;
+        //if counter...
+        winner = isThereAWinner(player, matrix);
+        // if end
+        //counter
+        // if counter 9 ... break
       }
-      //check if current user wins
-      //}
+      displayMatrix(matrix);
+      if (wasUpdated && !winner) {
+        // if(player === payers[0]){
+        //player = players[1]
+        // }else{
+        // player = players[0]
+        // }
+        player = player === players[0] ? players[1] : players[0];
+      }
+    } else {
+      console.log(errors);
     }
-    winner = true;
   }
+  console.log(`the winner is player ${player}`);
 };
 main();
 //displayMatrix(matrix);
