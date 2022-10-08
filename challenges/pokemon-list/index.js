c8567e64857d818e0280e247d8f09e4503eb9dde
Rick1196 <ricardompp11@gmail.com>
2022-10-07 22:04:33 -0500

    Co-authored-by: moscosa <moscosa@users.noreply.github.com>

--- challenges/pokemon-list/index.js
+++ challenges/pokemon-list/index.js
@@ -37,37 +37,77 @@ const container = createElement({
 });
 
 const input = createElement({
-    elementType : "input",
-    class: ["form-input"],
-    id: ['input'],
-    placeholder: ['Send'],
+  elementType: "input",
+  class: ["form-input"],
+  id: ["input"],
+  placeholder: ["Send"],
 });
 
 const button = createElement({
-    elementType : 'button',
-    id : ['sumbmit'],
-    innerText : ['Send', 'innerText'],
+  elementType: "button",
+  id: ["sumbmit"],
+  innerText: ["Send", "innerText"],
+  onclick: [
+    () => {
+      handleOnClick("input", "list");
+    },
+    "onclick",
+  ],
 });
 
 const list = createElement({
-    elementType: 'ul',
-    id : ['list'],
+  elementType: "ul",
+  id: ["list"],
 });
 
 const listTitle = createElement({
-    elementType: 'p',
-    innerText : ['Pokemons List', 'innerText'],
+  elementType: "p",
+  innerText: ["Pokemons List", "innerText"],
 });
 
 const listContainer = createElement({
-    elementType: 'div',
-    id: ['lisContainer'],
-    class: ['container'],
+  elementType: "div",
+  id: ["lisContainer"],
+  class: ["container"],
 });
 
 list.appendChild(listTitle);
 listContainer.appendChild(list);
 container.appendChild(input);
 container.appendChild(button);
+b;
 root.appendChild(container);
 root.appendChild(listContainer);
+
+const handleOnClick = async (idInput, idList) => {
+  const input = document.getElementById(idInput);
+  const list = document.getElementById(idList);
+  const newInput = input.value;
+  if (newInput && newInput.length > 0) {
+    const poke = await fetchPokemon(newInput);
+    if (poke) {
+      const li = createElement({
+        elementType: "li",
+        innerText: [newInput, "innerText"],
+      });
+      list.appendChild(li);
+    } else {
+      alert("Invalid Pokemon");
+    }
+  } else {
+    alert("Invalid String");
+  }
+};
+
+const fetchPokemon = async (pokemon) => {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    );
+    return await response.json();
+  } catch {
+    return null;
+  }
+  // if error => response = ""
+  // if not error => reponse = {}
+};
